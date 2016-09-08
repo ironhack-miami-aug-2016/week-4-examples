31f4896d85e2572f29e6682b0fdfd2c1d38eac72
Leo Correa <lcorr005@gmail.com>
2016-09-08 15:57:57 -0400

    New time entries on projects

--- timetracking/app/controllers/time_entries_controller.rb
+++ timetracking/app/controllers/time_entries_controller.rb
@@ -12,4 +12,32 @@ class TimeEntriesController < ApplicationController
       date: Time.now.beginning_of_month..Time.now.end_of_month
     )
   end
+
+  def new
+    # Input
+    # - params[:project_id]
+    # 1. Find the project
+    # 2. Instantiate new time entry for the project
+    @project = Project.find(params[:project_id])
+    @time_entry = @project.time_entries.new
+  end
+
+  def create
+    # Input
+    # - params[:project_id]
+    # - params[:time_entry]
+    # 1. Find the project
+    # 2. instantiate the new time entry with time_entry params
+    # 3. save the time entry
+    # 4. redirect back project's time entries
+    project = Project.find(params[:project_id])
+    time_entry = project.time_entries.new(
+      minutes: params[:time_entry][:minutes],
+      hours: params[:time_entry][:hours],
+      date: params[:time_entry][:date]
+    )
+    time_entry.save
+
+    redirect_to "/projects/#{project.id}/time_entries"
+  end
 end
