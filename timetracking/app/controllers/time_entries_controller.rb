fa7dc6db00b1c1a5f1919e7ef97922b99c482c39
Leo Correa <lcorr005@gmail.com>
2016-09-09 12:31:39 -0400

    Updating time entries and strong params

--- timetracking/app/controllers/time_entries_controller.rb
+++ timetracking/app/controllers/time_entries_controller.rb
@@ -22,6 +22,16 @@ class TimeEntriesController < ApplicationController
     @time_entry = @project.time_entries.new
   end
 
+  def edit
+    # Input
+    # - params[:project_id]
+    # - params[:id]
+    # Ouput
+    # - The edit form
+    @project = Project.find(params[:project_id])
+    @time_entry = @project.time_entries.find(params[:id])
+  end
+
   def create
     # Input
     # - params[:project_id]
@@ -31,13 +41,37 @@ class TimeEntriesController < ApplicationController
     # 3. save the time entry
     # 4. redirect back project's time entries
     project = Project.find(params[:project_id])
-    time_entry = project.time_entries.new(
-      minutes: params[:time_entry][:minutes],
-      hours: params[:time_entry][:hours],
-      date: params[:time_entry][:date]
-    )
+    time_entry = project.time_entries.new(time_entry_params)
     time_entry.save
 
     redirect_to "/projects/#{project.id}/time_entries"
   end
+
+  def update
+    project = Project.find(params[:project_id])
+    time_entry = project.time_entries.find(params[:id])
+
+    if time_entry.update(time_entry_params)
+      redirect_to "/projects/#{project.id}/time_entries"
+    else
+      redirect_to "/projects/#{project.id}/time_entries/#{time_entry.id}/edit"
+    end
+  end
+
+  private
+
+  def time_entry_params
+    params.require(:time_entry).permit(:hours, :minutes, :date)
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
--- timetracking/app/models/project.rb
+++ timetracking/app/models/project.rb
@@ -1,3 +1,8 @@
 class Project < ApplicationRecord
   has_many :time_entries
+
+  def self.clean_old
+    where("created_at > ?", 7.days.ago).destroy_all
+  end
 end
+
