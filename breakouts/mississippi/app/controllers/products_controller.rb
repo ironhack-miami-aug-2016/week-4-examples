7043efa5a6834a8816ad596fbf025fcd0bbb1a7f
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-09-13 18:21:16 -0400

    Mississippi: Add new/create pages

--- breakouts/mississippi/app/controllers/products_controller.rb
+++ breakouts/mississippi/app/controllers/products_controller.rb
@@ -4,4 +4,31 @@ class ProductsController < ApplicationController
 
     render :index
   end
+
+  def new
+    @the_product = Product.new
+
+    render :new
+  end
+
+
+  def create
+    the_product = Product.new(product_params)
+
+    if the_product.save
+      redirect_to products_path
+    else
+      @the_product = the_product
+
+      render :new
+    end
+  end
+
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :image, :description)
+  end
 end
