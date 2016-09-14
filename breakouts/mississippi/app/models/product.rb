530766e685d19a188d91dde56c62221135acc94f
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-09-14 17:47:23 -0400

    Mississippi: List reviews in product details

--- breakouts/mississippi/app/models/product.rb
+++ breakouts/mississippi/app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, numericality: true
+
+  has_many :reviews
 end
--- breakouts/mississippi/db/schema.rb
+++ breakouts/mississippi/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913210448) do
+ActiveRecord::Schema.define(version: 20160914213348) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20160913210448) do
     t.string   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "message"
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
 end
